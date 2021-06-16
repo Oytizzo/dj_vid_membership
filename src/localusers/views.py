96a78d05159517ac5db016ade0939fbffb56d4ec
Oytizzo <oytizzo@gmail.com>
2021-06-16 23:04:14 +0600

    Add login_required in home view

--- src/localusers/views.py
+++ src/localusers/views.py
@@ -3,5 +3,6 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
 
+@login_required
 def home(request):
     return HttpResponse("<h1>Hello world!</h1>")
