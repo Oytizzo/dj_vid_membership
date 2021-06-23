f852ca27ae99d0fd408284df5f34e56ad79599d5
Oytizzo <oytizzo@gmail.com>
2021-06-23 23:49:52 +0600

    Add user_logged_out signals

--- src/localusers/signals.py
+++ src/localusers/signals.py
@@ -1,4 +1,4 @@
-from allauth.account.signals import user_logged_in
+from allauth.account.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
 
 
@@ -7,3 +7,8 @@ def logged_in(request, user, **kwargs):
     print("user_logged_in signals working just nice. I'm from apps.py > signals.py")
 
 # user_logged_in.connect(logged_in)
+
+
+@receiver(user_logged_out)
+def logged_out(request, user, **kwargs):
+    print("You are just logged out! Log in again?")
