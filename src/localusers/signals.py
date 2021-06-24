3c988cd8891d85dde5c23be904af4ee1d08464b2
Oytizzo <oytizzo@gmail.com>
2021-06-24 22:42:32 +0600

    Add signed_up signal

--- src/localusers/signals.py
+++ src/localusers/signals.py
@@ -1,4 +1,4 @@
-from allauth.account.signals import user_logged_in, user_logged_out
+from allauth.account.signals import user_logged_in, user_logged_out, user_signed_up
 from django.dispatch import receiver
 
 
@@ -12,3 +12,8 @@ def logged_in(request, user, **kwargs):
 @receiver(user_logged_out)
 def logged_out(request, user, **kwargs):
     print("You are just logged out! Log in again?")
+
+
+@receiver(user_signed_up)
+def signed_up(request, user):
+    print("You are just signed up! Log in?")
