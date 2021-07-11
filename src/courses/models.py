3d591c6df9e7b7a61a7a3dddcbf38324c17b9a25
Oytizzo <oytizzo@gmail.com>
2021-07-12 00:18:56 +0600

    Slugify Course and Lesson automatically

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,14 +1,27 @@
+import string
+import random
+
 from django.db import models
+from django.utils.text import slugify
+
+
+def rand_slug():
+    return ''.join(random.choice(string.ascii_letters) for _ in range(6))
 
 
 class Course(models.Model):
-    slug = models.SlugField()
+    slug = models.SlugField(blank=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
     # allowed_memberships = models.ManyToManyField
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(rand_slug() + "-" + self.title)
+        super().save(*args, **kwargs)
 
 
 class Lesson(models.Model):
@@ -22,3 +35,8 @@ class Lesson(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(rand_slug() + "-" + self.title)
+        super().save(*args, **kwargs)
