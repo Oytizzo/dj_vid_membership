fffddc02006b2a656c86516ec8fc67fb061aaa20
Oytizzo <oytizzo@gmail.com>
2021-07-11 20:47:18 +0600

    Add Course and Lesson models

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    # allowed_memberships = models.ManyToManyField
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    slug = models.SlugField()
+    position = models.IntegerField()
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
+    video = models.CharField(max_length=255)
+    thumbnail = models.ImageField()
+
+    def __str__(self):
+        return self.title
