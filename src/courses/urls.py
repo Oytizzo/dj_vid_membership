626c5655625f4fb2eb510ec3eaae1058b6d816f8
Oytizzo <oytizzo@gmail.com>
2021-07-17 23:04:23 +0600

    Add CourseListView

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.courses, name='course-list'),
+    # path('', views.courses, name='course-list'),
+    path('', views.CourseListView.as_view(), name='course-list'),
 ]
--- src/courses/views.py
+++ src/courses/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.http import HttpResponse
+
+from .models import Course, Lesson
 
 
 def courses(request):
     context = {}
     return render(request, 'courses/home.html', context)
+
+
+class CourseListView(ListView):
+    model = Course
