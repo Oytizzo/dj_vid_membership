6f4173860797918631f0b1e5109325e49000b775
Oytizzo <oytizzo@gmail.com>
2021-07-18 23:04:41 +0600

    Add Membership model

--- src/memberships/models.py
+++ src/memberships/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Memberships(models.Model):
+    slug = models.SlugField()
+    membership_type = models.CharField(max_length=25)
+    price = models.IntegerField()
+    strip_plan_id = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.membership_type
